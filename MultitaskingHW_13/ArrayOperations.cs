f6a59c1f11349722eed164d9e87eaa300fc645ba
Zoriana Zhuk <zoriana.zh08@gmail.com>
2024-09-11 20:58:40 +0300

    HomeWork13

---
+++ MultitaskingHW_13/ArrayOperations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class ArrayOperations
+{
+    private int[] array;
+
+    public ArrayOperations(int[] array)
+    {
+        this.array = array;
+    }
+
+    public int FindMin()
+    {
+        int min = array[0];
+        foreach (var num in array)
+        {
+            if (num < min)
+                min = num;
+        }
+        return min;
+    }
+
+    public int FindMax()
+    {
+        int max = array[0];
+        foreach (var num in array)
+        {
+            if (num > max)
+                max = num;
+        }
+        return max;
+    }
+
+    public int CalculateSum()
+    {
+        int sum = 0;
+        foreach (var num in array)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public double CalculateAverage()
+    {
+        return CalculateSum() / (double)array.Length;
+    }
+}
---
+++ MultitaskingHW_13/TextOperations.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+class TextOperations
+{
+    private string text;
+
+    public TextOperations(string filePath)
+    {
+        this.text = File.ReadAllText(filePath);
+    }
+
+    public Dictionary<char, int> GetCharFrequency()
+    {
+        Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+        foreach (char c in text)
+        {
+            if (frequency.ContainsKey(c))
+                frequency[c]++;
+            else
+                frequency[c] = 1;
+        }
+        return frequency;
+    }
+
+    public Dictionary<string, int> GetWordFrequency()
+    {
+        Dictionary<string, int> frequency = new Dictionary<string, int>();
+        string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string word in words)
+        {
+            if (frequency.ContainsKey(word))
+                frequency[word]++;
+            else
+                frequency[word] = 1;
+        }
+        return frequency;
+    }
+}
